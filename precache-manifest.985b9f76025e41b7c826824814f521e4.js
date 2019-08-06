94e9d7c1d467daddc073f5e6b93f8373d2196047
ronyshchenko <roma.onyshchenko@gmail.com>
2019-08-06 15:48:18 +0300

    Updates

---
+++ precache-manifest.985b9f76025e41b7c826824814f521e4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a45df35117cbb11b61fca4e1a1023f2c",
+    "url": "/react_dinamic-list-of-postlist-of-posts/index.html"
+  },
+  {
+    "revision": "0eb2c032aacc1efdb792",
+    "url": "/react_dinamic-list-of-postlist-of-posts/static/css/main.534028cb.chunk.css"
+  },
+  {
+    "revision": "9ab2fa6de350a941bcd0",
+    "url": "/react_dinamic-list-of-postlist-of-posts/static/js/2.65c4bffa.chunk.js"
+  },
+  {
+    "revision": "0eb2c032aacc1efdb792",
+    "url": "/react_dinamic-list-of-postlist-of-posts/static/js/main.ed3b01ea.chunk.js"
+  },
+  {
+    "revision": "f7940b8c9597c7f375b5",
+    "url": "/react_dinamic-list-of-postlist-of-posts/static/js/runtime~main.0eb88d3d.js"
+  }
+]);
